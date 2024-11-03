1e6351fcd06e512c8676aceca5d13c08f6c7317a
Dang Huy <huydang2132>
2024-11-03 20:23:10 +0700

    add axios

--- client/src/views/layouts/footer/TheFooter.vue
+++ client/src/views/layouts/footer/TheFooter.vue
@@ -4,8 +4,26 @@
 
 <template>
     <footer>
-        Footer
+        <div class="footer--copyright">
+            &copy; 2024 Copyright by <a href="https://github.com/huydang2132">Dang Huy</a>
+        </div>
     </footer>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+footer {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    background-color: var(--color-primary);
+    color: var(--color-white);
+}
+
+footer .footer--copyright a {
+    color: var(--color-white);
+    text-decoration: none;
+}
+</style>
